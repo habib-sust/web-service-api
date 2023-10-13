1c9bc3f0afe04efc84ddddca8a3814c376bd2ec3
habib-sust <habibur.sust@gmail.com>
2023-10-13 22:54:09 +0600

    Implement 2 new api to add a new album and get an album by id.

--- main.go
+++ main.go
@@ -9,7 +9,11 @@ import (
 
 func main() {
 	router := gin.Default()
+
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+
 	router.Run("localhost:8080")
 }
 
@@ -31,4 +35,35 @@ var albums = []models.Album{
 // getAlbums responds with the list of all albums as JSON
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body
+func postAlbums(c *gin.Context) {
+	var newAlbum models.Album
+
+	// Call BindJSON to bind the received JSON to newAlbum
+	if error := c.BindJSON(&newAlbum); error != nil {
+		return
+	}
+
+	// Add the new album to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// paramter send by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// parameter send by the client, then returns that album as a response.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
