d139649f05e1f394694956ebaaa6bc5fb608b304
habib-sust <habibur.sust@gmail.com>
2023-10-13 21:27:45 +0600

    Implement `get albums` api end-point
    * Add Album moedel
    * Implement gin for web service

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"habib/web-service-api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("localhost:8080")
+}
+
+// /*
+// 	/albums
+// 		# GET - get a list of all albums, returned as JSON
+// 		# POST - add a new album from the request data send as JSON
+
+// 	/albums/id
+// 		# GET - get an album by its ID, returning the album data as JSON
+// */
+
+var albums = []models.Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 36.99},
+}
+
+// getAlbums responds with the list of all albums as JSON
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
