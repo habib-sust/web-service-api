25e4c6956f97d96232432ae9779edecaf68773cd
habib-sust <habibur.sust@gmail.com>
2023-10-16 21:53:57 +0600

    Update response_util

---
+++ app/pkg/response_util.go
@@ -0,0 +1,26 @@
+package pkg
+
+import (
+	"habib/web-service-api/app/constant"
+	"habib/web-service-api/app/domain/dto"
+)
+
+func Null() interface{} {
+	return nil
+}
+
+func BuildResponse[T any](responseStatus constant.ResponseStatus, data T) dto.ApiResponse[T] {
+	return BuildResponse_(
+		responseStatus.GetResponseStatus(),
+		responseStatus.GetResponseMessage(),
+		data,
+	)
+}
+
+func BuildResponse_[T any](status string, message string, data T) dto.ApiResponse[T] {
+	return dto.ApiResponse[T]{
+		ResponseKey:     status,
+		ResponseMessage: message,
+		Data:            data,
+	}
+}
